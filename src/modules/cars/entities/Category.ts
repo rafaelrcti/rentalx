f375955e6ac8aa221c75d24efa6844386381ff5b
rafaelrcti <rafarc19@gmail.com>
2021-05-10 21:33:53 -0300

    Montando conexao no banco

---
+++ src/modules/cars/entities/Category.ts
@@ -0,0 +1,26 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+import { v4 as uuidv4 } from 'uuid';
+
+@Entity("categories")
+class Category {
+
+@PrimaryColumn()    
+id:String;
+
+@Column()
+name:String;
+
+@Column()
+description:String;
+
+@CreateDateColumn()
+created_at: Date;
+
+    constructor(){
+        if(!this.id){
+            this.id = uuidv4();
+        }
+    }
+}
+
+export {Category}
